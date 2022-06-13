c5b728c2351bed1f344d008cd4e2845c6f83e808
Sushidu3630 <103418761+Sushidu3630@users.noreply.github.com>
2022-06-13 11:09:05 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P14, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P14, 0)
+})
